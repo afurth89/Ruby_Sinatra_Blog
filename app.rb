c4dd6718a89b228be2b9703b85c811eb41f4dc0b
Andrew Furth <andrew.furth@gmail.com>
2016-08-16 11:01:36 -0700

    homepage loads all posts

--- app.rb
+++ app.rb
@@ -4,6 +4,23 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
 
-
 class Post < ActiveRecord::Base
+end
+
+
+helpers do
+  def title
+    if @title
+      "#{@title}"
+    else
+      "Welcome"
+    end
+  end
+end
+
+get "/" do
+  @posts = Post.order("created_at DESC")
+  @title = "Welcome"
+  erb :"posts/index"
+  # "Hello World"
 end
