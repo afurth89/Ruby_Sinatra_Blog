260f4f25b46328d1fc04231eca4a74a338123213
Andrew Furth <andrew.furth@gmail.com>
2016-08-16 11:21:25 -0700

    create post routing and view added; new post added to db successfully

--- app.rb
+++ app.rb
@@ -25,6 +25,22 @@ get "/" do
   # "Hello World"
 end
 
+
+get "/posts/create" do
+  @title = "Create post"
+  @post = Post.new 
+  erb :"posts/create"
+end
+
+post "/posts" do
+  @post = Post.new(params[:post])
+  if @post.save
+    redirect "posts/#{@post.id}"
+  else
+    erb :"posts/create"
+  end
+end
+
 get "/posts/:id" do
   @post = Post.find(params[:id])
   @title = @post.title
